8c7aaa2b09e19890bc02dd9981886e77c7a1339c
Pablo Miguel Angel Curtti <156843294+PMAC80@users.noreply.github.com>
2024-06-22 19:27:31 -0300

    Update ejercicio1.cpp
    saque algunos comentarios obsoletos

--- ejercicio1/ejercicio1.cpp
+++ ejercicio1/ejercicio1.cpp
@@ -1,20 +1,24 @@
-#include <iostream> // Incluye la librería iostream para la entrada y salida de datos
+#include <iostream> 
+using namespace std;
 
-int main() { // Función principal donde comienza la ejecución del programa
-    const int SIZE = 100; // Define una constante llamada SIZE con valor 100
-    int numbers[SIZE]; // Declara un array de enteros llamado 'numbers' con tamaño 'SIZE'
+int main() 
+{ 
+    const int SIZE = 100;
+    int numbers[SIZE];
 
     // Bucle for para llenar el array con los primeros 100 números enteros
-    for (int i = 0; i < SIZE; ++i) {
+    for (int i = 0; i < SIZE; ++i) 
+    {
         numbers[i] = i + 1; // Asigna a cada elemento del array el valor de 'i + 1'
     }
 
     // Bucle for para mostrar los números en pantalla
-    for (int i = 0; i < SIZE; ++i) {
-        std::cout << numbers[i] << " "; // Imprime cada número seguido de un espacio
+    for (int i = 0; i < SIZE; ++i) 
+    {
+       cout << numbers[i] << " "; // Imprime cada número seguido de un espacio
     }
 
-    std::cout << std::endl; // Imprime una nueva línea al final para mejorar el formato de salida
+    cout << endl; // Imprime una nueva línea al final para mejorar el formato de salida
 
-    return 0; // Termina la función main y devuelve 0, indicando que el programa finalizó correctamente
+    return 0; 
 }
