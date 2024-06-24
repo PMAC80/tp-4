e052d0ac9ba5eecc2718bb6d2cca8e2e233d0eb6
Pablo Miguel Angel Curtti <156843294+PMAC80@users.noreply.github.com>
2024-06-24 03:11:08 -0300

    Update ejercicio3.cpp
    otro avance de la ciencia

--- ejercicio3/ejercicio3.cpp
+++ ejercicio3/ejercicio3.cpp
@@ -1,32 +1,52 @@
-#include <iostream> // Incluye la librería iostream para la entrada y salida de datos
+/*Realizar un Programa que rellene un array con los números impares comprendidos entre 1 y
+100 y los muestre en pantalla*/
 
-// Función que determina si un número es primo
-bool esPrimo(int num) {
-    if (num <= 1) return false; // Los números menores o iguales a 1 no son primos
-    for (int i = 2; i * i <= num; ++i) { // Bucle que comprueba los posibles divisores del número
-        if (num % i == 0) return false; // Si encuentra un divisor, no es primo
+#include <iostream>
+using namespace std;
+
+void clearConsole();
+bool numeroImpar(int numero);
+
+int main()
+{
+    clearConsole();
+    int dim = 100;
+    typedef int tnumerosImpares[dim];
+    tnumerosImpares numimpar;
+
+    for (int i = 1; i <= dim; i++)
+    {
+        if (numeroImpar(i))
+            {
+                numimpar[i] = i;
+                cout << "es numeros impar : "<< endl;
+                cout << numimpar[i] << endl;
+            }
     }
-    return true; // Si no encuentra divisores, es primo
+    return 0;    
 }
 
-int main() { // Función principal donde comienza la ejecución del programa
-    const int SIZE = 100; // Define una constante llamada SIZE con valor 100
-    int primos[SIZE]; // Declara un array de enteros llamado 'primos' con tamaño 'SIZE'
-    int index = 0; // Índice para mantener la posición actual en el array 'primos'
-
-    // Bucle para encontrar los números primos entre 1 y 100
-    for (int i = 1; i <= 100; ++i) {
-        if (esPrimo(i)) { // Comprueba si el número es primo
-            primos[index++] = i; // Si es primo, lo añade al array 'primos' y aumenta el índice
+bool numeroImpar( int numero)
+{
+    for (int i = 0; i < dim; i++)
+    {
+        if (numero % 2 == 1) 
+        {
+           return true;
         }
+        return false;
     }
-
-    // Bucle para mostrar los números primos en pantalla
-    for (int i = 0; i < index; ++i) {
-        std::cout << primos[i] << " "; // Imprime cada número primo seguido de un espacio
-    }
-
-    std::cout << std::endl; // Imprime una nueva línea al final para mejorar el formato de salida
-
-    return 0; // Termina la función main y devuelve 0, indicando que el programa finalizó correctamente
 }
+
+ void clearConsole() 
+{
+    #ifdef __unix__
+        system("clear");
+    #elif __APPLE__
+        system("clear");
+    #elif defined(_WIN32) || defined(WIN64)
+        system("cls");
+    #endif
+}
+
+     
